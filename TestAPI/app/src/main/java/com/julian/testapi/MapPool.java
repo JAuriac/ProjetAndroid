e70d305cbeb8e0e7a1e76a7625f00c14340e0375
JAuriac <56091659+JAuriac@users.noreply.github.com>
2020-03-24 01:32:55 +0100

    Sixième commit : Map Pool vide et format tablette

---
+++ TestAPI/app/src/main/java/com/julian/testapi/MapPool.java
@@ -0,0 +1,12 @@
+package com.julian.testapi;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MapPool extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main); Ici tu devrais pouvoir mettre la grid à la place d'activity_main.xml
+    }
+}
